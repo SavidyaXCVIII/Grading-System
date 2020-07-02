ee4b67f89e83b096f67d896f4fb2e3f8a1c7a471
Savidya Udesh <45062811+SavidyaXCVIII@users.noreply.github.com>
2020-07-02 16:46:24 +0530

    added pojo classes of Question and Assignment

--- backend/src/main/java/com/gs/backend/model/Assignment.java
+++ backend/src/main/java/com/gs/backend/model/Assignment.java
@@ -1,5 +1,21 @@
 package com.gs.backend.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashMap;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Assignment {
 
+    private String assignmentName;
+    private double timeSpent;
+    private int numberOfQuestions;
+    private int numberOfAttempts;
+    private Question[] questions;
 }
---
+++ backend/src/main/java/com/gs/backend/model/Question.java
@@ -0,0 +1,9 @@
+package com.gs.backend.model;
+
+public class Question {
+
+    private int questionNumber;
+    private String[] answers;
+    private int[] correctAnswers;
+
+ }
